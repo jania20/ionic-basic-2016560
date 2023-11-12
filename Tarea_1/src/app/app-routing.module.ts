453a84bf6c54141339695695da8e1b2d1977d89a
Patricio <jania20@gmail.com>
2023-11-11 23:50:28 -0600

    Practica 11

--- Tarea_1/src/app/app-routing.module.ts
+++ Tarea_1/src/app/app-routing.module.ts
@@ -38,7 +38,10 @@ const routes: Routes = [
       {
         path: 'alumnos',
         loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
-      }
+      },
+      {
+        path: 'destinos-api',
+        loadChildren: () => import('./destinos-api/destinos-api.module').then( m => m.DestinosApiPageModule)
     ],
     canActivate: [AutGuardGuard]
   },
@@ -50,7 +53,6 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   }
-  
 ];
 
 @NgModule({
